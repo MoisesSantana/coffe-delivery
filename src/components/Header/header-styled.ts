e10f41f4d9a8724fde19bca14c3490b8eaf9b64d
Moises Santana <moisaant@gmail.com>
2023-03-26 13:50:35 -0300

    :lipstick: estilização do Header.

---
+++ src/components/Header/header-styled.ts
@@ -0,0 +1,42 @@
+import { styled } from '@mui/material';
+
+export const HeaderContainer = styled('header')`
+  width: 100vw;
+  background-color: #fafafa;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 4rem;
+  height: 105px;
+  align-items: center;
+  @media (max-width: 768px) {
+    padding: 0 2rem;
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    height: 38px;
+    justify-content: space-between;
+
+    div {
+      padding: 8px;
+      box-sizing: border-box;
+      background-color: orange;
+      display: flex;
+      gap: 4px;
+      align-items: center;
+      border-radius: 6px;
+      height: 100%;
+      color: #4b2995;
+    }
+
+    .location {
+      background-color: #ebe5f9;
+    }
+
+    .cart {
+      background-color: #f1e9c9;
+    }
+  }
+`;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
-import Logo from '../../assets/logo.svg';
-import Pin from '../../assets/pin.svg';
-import Cart from '../../assets/cart.svg';
+import Logo from '../../assets/images/logo.svg';
+import Pin from '../../assets/images/pin.svg';
+import Cart from '../../assets/images/cart.svg';
+import { HeaderContainer } from './header-styled';
 
 export function Header() {
   return (
-    <header>
+    <HeaderContainer>
       <img src={Logo} alt="Coffe Delivery" />
-      <section>
-        <div>
+      <section className="actions">
+        <div className="location">
           <img src={Pin} alt="localização" />
           <span>Rio de Janeiro</span>
         </div>
-        <div>
+        <div className="cart">
           <img src={Cart} alt="carrinho" />
         </div>
       </section>
-    </header>
+    </HeaderContainer>
   );
 }
