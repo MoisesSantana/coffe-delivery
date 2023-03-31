59ef448253860bf9bdd8eed2e7dddb5eb0b7bdf6
Moises Santana <moisaant@gmail.com>
2023-03-31 16:43:52 -0300

    :bug: fix: correção do background da aplicação.

---
+++ src/Layouts/DefaultLayout/default-layout-styled.ts
@@ -0,0 +1,6 @@
+import { styled } from '@mui/material';
+
+export const LayoutContainer = styled('div')`
+  background-color: #fafafa;
+  width: 100%;
+`;
--- src/Layouts/DefaultLayout/index.tsx
+++ src/Layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../components/Header';
+import { LayoutContainer } from './default-layout-styled';
 
 export function DefaultLayout() {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   );
 }
