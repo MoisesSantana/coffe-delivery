cabf7fa81d689d2739259984a5c12d5fd16c6eb9
Moises Santana <moisaant@gmail.com>
2023-04-26 22:16:26 -0300

    :recycle: definindo theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,57 +1,81 @@
 import { createTheme } from '@mui/material/styles';
 
-// export const customPallet = {
-//   yellow: {
-//     main: '#DBAC2C',
-//     dark: '#C47F17',
-//     light: '#F1E9C9',
-//   },
-//   purple: {
-//     main: '#8047F8',
-//     dark: '#4B2995',
-//     light: '#EBE5F9',
-//   },
-//   base: {
-//     title: '#272221',
-//     subtitle: '#403937',
-//     text: '#574f4d',
-//     label: '#8d8686',
-//     hover: '#d7d5d5',
-//     button: '#e6e5e5',
-//     input: '#ededed',
-//     card: '#f3f2f2',
-//     background: '#fafafa',
-//     white: '#fff',
-//   },
-// };
-
-export const customPalette = {
-  // yellowMain: '#DBAC2C',
-  // yellowDark: '#C47F17',
-  // YellowLight: '#F1E9C9',
-  yellow: {
-    main: '#DBAC2C',
-    dark: '#C47F17',
-    light: '#F1E9C9',
-  },
-  purpleMain: '#8047F8',
-  purpleDark: '#4B2995',
-  purpleLight: '#EBE5F9',
-  baseTitle: '#272221',
-  baseSubtitle: '#403937',
-  baseText: '#574f4d',
-  baseLabel: '#8d8686',
-  baseHover: '#d7d5d5',
-  baseButton: '#e6e5e5',
-  baseInput: '#ededed',
-  baseCard: '#f3f2f2',
-  baseBackground: '#fafafa',
-  baseWhite: '#fff',
-};
-
 const theme = createTheme({
   palette: {
-    ...customPalette,
+    warning: {
+      light: '#F1E9C9',
+      main: '#DBAC2C',
+      dark: '#C47F17',
+    },
+    secondary: {
+      light: '#EBE5F9',
+      main: '#8047F8',
+      dark: '#4B2995',
+    },
+    success: {
+      light: '#1dd1a1',
+      main: '#10ac84',
+      dark: '#01a3a4',
+    },
+    grey: {
+      50: '#FFFFFF',
+      100: '#FAFAFA',
+      200: '#F3F2F2',
+      300: '#EDEDED',
+      400: '#E6E5E5',
+      500: '#D7D5D5',
+      600: '#8D8686',
+      700: '#574F4D',
+      800: '#403937',
+      900: '#272221',
+    },
+    background: {
+      default: '#FAFAFA',
+    },
+    common: {
+      black: '#000000',
+      white: '#FFFFFF',
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: '"Baloo 2", cursive',
+      color: '#272221',
+      fontSize: 48,
+    },
+    h2: {
+      fontFamily: '"Baloo 2", cursive',
+      color: '#403937',
+      fontSize: 32,
+    },
+    h3: {
+      fontFamily: '"Baloo 2", cursive',
+      color: '#403937',
+      fontSize: 20,
+    },
+    h4: {
+      fontFamily: '"Baloo 2", cursive',
+      color: '#403937',
+      fontSize: 18,
+    },
+    caption: {
+      fontFamily: '"Roboto", sans-serif',
+      color: '#8D8686',
+      fontStyle: 'italic',
+    },
+    subtitle1: {
+      fontFamily: '"Roboto", sans-serif',
+      color: '#403937',
+      fontSize: 20,
+    },
+    body1: {
+      fontFamily: '"Roboto", sans-serif',
+      color: '#574F4D',
+    },
+    body2: {
+      ly: '"Roboto", sans-serif',
+      color: '#8d8686',
+    },
   },
 });
 
