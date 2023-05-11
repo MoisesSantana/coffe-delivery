ed39add7b11a57b42ac02ddd3366916813ff175b
Moises Santana <moisaant@gmail.com>
2023-05-11 01:47:04 -0300

    :bug: adicionando estilizacao retirada erroneamente

--- src/styles/shared-styles.ts
+++ src/styles/shared-styles.ts
@@ -3,6 +3,13 @@ import { Box, styled } from '@mui/material';
 export const FormCards = styled(Box)`
   display: flex;
   flex-direction: column;
+  padding: 40px;
   background-color: ${({ theme }) => theme.palette.grey[200]};
   border-radius: 6px;
+
+  header {
+    display: flex;
+    align-items: flex-start;
+    gap: 6px;
+  }
 `;
