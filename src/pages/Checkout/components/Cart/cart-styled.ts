a22ed19cfb46a86445ae7f2d87333bb861afd8fc
Moises Santana <moisaant@gmail.com>
2023-05-12 00:30:47 -0300

    :construction: estabelecendo layout dos cards do carrinho

--- src/pages/Checkout/components/Cart/cart-styled.ts
+++ src/pages/Checkout/components/Cart/cart-styled.ts
@@ -1,21 +1,10 @@
-import { styled } from '@mui/material';
-import { CheckoutCards } from '../../../../styles/shared-styles';
+import { Button, styled } from '@mui/material';
 
 export const CartContainer = styled('aside')`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  /* justify-content: space-between; */
-  /* @media (max-width: 1820px) {
-    width: 70%;
-  }
-  @media (max-width: 1540px) {
-    width: 80%;
-  }
-  @media (max-width: 1360px) {
-    width: 90%;
-  } */
 `;
 
 export const CartProductList = styled('ul')`
@@ -31,15 +20,43 @@ export const CartProductList = styled('ul')`
     width: 368px;
     padding: 40px 0;
     border-bottom: 1px solid ${({ theme }) => theme.palette.grey[400]};
+    display: flex;
+    justify-content: space-between;
 
     &:first-child {
       padding-top: 0;
     }
+
+    img {
+      width: 64px;
+    }
+
+    .teste {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      padding: 0 1.5rem;
+      justify-content: space-between;
+
+      .teste2 {
+        display: flex;
+        gap: 0.5rem;
+      }
+    }
   }
 `;
 
-export const Fodase = styled('div')`
-  width: 368px;
-  padding: 40px 0;
-  background-color: red;
+export const RemoveButtonContainer = styled(Button)`
+  border: none;
+  background-color: ${({ theme }) => theme.palette.grey[400]};
+  color: ${({ theme }) => theme.palette.grey[700]};
+  font-weight: normal;
+  padding: 0 10px;
+  font-size: 0.75rem;
+  font-family: 'Roboto', sans-serif;
+
+  img {
+    max-width: 16px;
+    margin-right: 6px;
+  }
 `;
--- src/pages/Checkout/components/Cart/index.tsx
+++ src/pages/Checkout/components/Cart/index.tsx
@@ -1,9 +1,14 @@
 import { ButtonGroup, Typography } from '@mui/material';
-import { CartContainer, CartProductList } from './cart-styled';
+import {
+  CartContainer,
+  CartProductList,
+  RemoveButtonContainer,
+} from './cart-styled';
 import {
   ButtonContainer,
   CheckoutCards,
 } from '../../../../styles/shared-styles';
+import { cards } from '../../../../data';
 
 export function Cart() {
   return (
@@ -11,83 +16,30 @@ export function Cart() {
       <Typography variant="h4">Cafés selecionados</Typography>
       <CheckoutCards p="2.5rem">
         <CartProductList>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
-          <li>
-            <img src="/coffees/coffee-arabe.svg" alt="" />
-            <span>arabe</span>
-            <ButtonGroup>
-              <ButtonContainer>-</ButtonContainer>
-              <ButtonContainer>1</ButtonContainer>
-              <ButtonContainer>+</ButtonContainer>
-            </ButtonGroup>
-            <ButtonContainer>Remover</ButtonContainer>
-            <span>R$00,00</span>
-          </li>
+          {cards.map((coffee) => (
+            <li key={coffee.name}>
+              <img src={coffee.image} alt={coffee.name} />
+              <div className="teste">
+                <Typography variant="subtitle1" fontSize={16}>
+                  {coffee.name}
+                </Typography>
+                <div className="teste2">
+                  <ButtonGroup>
+                    <ButtonContainer>-</ButtonContainer>
+                    <ButtonContainer>1</ButtonContainer>
+                    <ButtonContainer>+</ButtonContainer>
+                  </ButtonGroup>
+                  <RemoveButtonContainer>
+                    <img src="/cart/trash.svg" alt="lixeira" />
+                    Remover
+                  </RemoveButtonContainer>
+                </div>
+              </div>
+              <Typography variant="body1" fontWeight="bold" fontSize={16}>
+                R$00,00
+              </Typography>
+            </li>
+          ))}
         </CartProductList>
       </CheckoutCards>
     </CartContainer>
